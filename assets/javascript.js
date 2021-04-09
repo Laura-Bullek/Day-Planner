e147fe9f72239b2935f05ac48ffbc4bc61dd8c20
LB Vik <laura.bullek@gmail.com>
2021-04-08 20:58:29 -0400

    Added assignment criteria and started pseudocode

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,5 +1,20 @@
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
+
 $(document).ready(function(){
     $("#currentDay").text(moment().format("MMMM Do YYYY"));
 
-    var currentTime = parseInt(moment().format("H"));
+    let currentTime = parseInt(moment().format("H"));
 });
+
